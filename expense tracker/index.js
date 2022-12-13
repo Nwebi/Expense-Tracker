5c53d36322151ba5a3e9fb6cd6f6e8f84b744475
Nwebi <107417579+Nwebi@users.noreply.github.com>
2022-12-13 03:06:17 -0800

    Add files via upload

---
+++ expense tracker/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import App from './App';
+//import MyApp from './MyApp';
+//import ExpenseApp from './expense tracker/ExpenseApp';
+import App from './Movie/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
